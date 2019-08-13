df6357dda123ee4e443d40133465539403f2a192
Daniela Salermi <51088476+DaniSalermi@users.noreply.github.com>
2019-08-12 21:56:41 -0400

    Feature/daniela salermi/complete Home Page (#11)
    * Reseteo password
    
    * Create home
    
    * Home page ready

--- src/route.js
+++ src/route.js
@@ -1,9 +1,16 @@
 import { templateLogin } from "./assets/views/templateLogin.js";
 import { templateCreate } from "./assets/views/templateCreate.js";
 import { templateFeed } from "./assets/views/templateFeed.js";
+import { templateResetPassword } from "./assets/views/templateResetPassword.js";
+import { templateSuccessCreate } from "./assets/views/templateSuccessCreate.js";
+import { templateHome } from "./assets/views/templateHome.js";
 import { templatePost } from "./assets/views/templatePost.js";
 
+
 const changeRouter = hash => {
+  if (hash === "#/home") {
+    return showTemplate(hash);
+  }
   if (hash === "#/login") {
     return showTemplate(hash);
   }
@@ -13,8 +20,15 @@ const changeRouter = hash => {
   if (hash === "#/feed") {
     return showTemplate(hash);
   }
+
+  if (hash === "#/resetPassword") {
+    return showTemplate(hash);
+  }
+  if (hash === "#/successCreate") {
+    return showTemplate(hash);
+  }
   if (hash === "#/post") {
-  return showTemplate(hash);
+    return showTemplate(hash);
   }
 };
 
@@ -23,6 +37,9 @@ const showTemplate = hash => {
   const containerRoot = document.getElementById("root");
   containerRoot.innerHTML = "";
   switch (router) {
+    case "home":
+      containerRoot.appendChild(templateHome());
+      break;
     case "login":
       containerRoot.appendChild(templateLogin());
       break;
@@ -32,8 +49,14 @@ const showTemplate = hash => {
     case "feed":
       containerRoot.appendChild(templateFeed());
       break;
+    case "resetPassword":
+      containerRoot.appendChild(templateResetPassword());
+      break;
+    case "successCreate":
+      containerRoot.appendChild(templateSuccessCreate());
     case "post":
       containerRoot.appendChild(templatePost());
+
       break;
     default:
       containerRoot.innerHTML = `<p>Error 404</p>`;
