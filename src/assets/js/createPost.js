391c92606ce6170eff20d56727e8ff9179d3fdd3
Daniela Salermi <51088476+DaniSalermi@users.noreply.github.com>
2019-08-14 14:50:40 -0400

    Feature/daniela salermi/profile (#12)
    * menu template
    
    * menu template
    
    Se crea el menu y se conecta a las paginas correspondientes

--- src/assets/js/createPost.js
+++ src/assets/js/createPost.js
@@ -6,21 +6,31 @@ export const createPost = () => {
   //guardamos firestore en variable
   let db = firebase.firestore();
   //guardamos fecha
-  let date = Date.now();
-  
+  let date = new Date();
+
   //guardamos os valores elijidos por el usuario
   let postCategory = document.querySelector("select[name=slctCategory]").value;
-          let postMesage = document.querySelector("textarea[name=postTxt]").value;
-          let postHashtags = document.querySelector("input[name=Hashtag]").value;
-          let postLocation = document.querySelector("input[name=location]").value;
-          console.log("category:", postCategory, "Mesage:", postMesage, "Hashtag:", postHashtags, "Location:", postLocation);
-//usamos esta funcion para obtener uid de uauario corriente
-          //firebase.auth().onAuthStateChanged(user => {
-    //obtenemos desde collecion users datos de usuario corriente con uid
-    //db.collection('users').doc(user.uid).get().then(doc => {
+  let postMesage = document.querySelector("textarea[name=postTxt]").value;
+  let postHashtags = document.querySelector("input[name=Hashtag]").value;
+  let postLocation = document.querySelector("input[name=location]").value;
+  console.log(
+    "category:",
+    postCategory,
+    "Mesage:",
+    postMesage,
+    "Hashtag:",
+    postHashtags,
+    "Location:",
+    postLocation
+  );
+  //usamos esta funcion para obtener uid de uauario corriente
+  //firebase.auth().onAuthStateChanged(user => {
+  //obtenemos desde collecion users datos de usuario corriente con uid
+  //db.collection('users').doc(user.uid).get().then(doc => {
 
-      //agraga un ID automatico
-    db.collection("posts").add({
+  //agraga un ID automatico
+  db.collection("posts")
+    .add({
       //db.collection("posts").doc(user.uid).set({
       //uid: user.uid,
       //author: user.email,
@@ -29,39 +39,37 @@ export const createPost = () => {
       category: postCategory,
       message: postMesage,
       hashtag: postHashtags,
-      location: postLocation,
+      location: postLocation
       // like: 1,
       // liked: 30
     })
     .then(function(docRef) {
       console.log("Document written with ID: ", docRef.id);
       alert("post guardado con exito");
-    window.location.hash='#/feed';
+      window.location.hash = "#/feed";
     })
     .catch(function(error) {
       console.error("Error adding document: ", error);
-    })
-   // })
+    });
+  // })
   //})
-  };
-    
-  // const splitHashtag = (postHashtags) => {
-    
-  //   let splitHashtagArray = postHashtags.split("#" || " ");
-  //   let hashtagSearch = {
-  //     firstHash: splitHashtagArray[0],
-  //     secondhash: splitHashtagArray[1],
-  //   };
-  //   return hashtagSearch;
-  // };
-  // //vay guardar usuario en la base de datos despues de logarse
-  // const saveUserToDatabaseAfterLogin = (user, userName) => {
-  //   //Convertir las informaciones de google en um objecto
-  //   db.collection("users").doc(user.uid).set({
-  //     email:user.email,
-  //     firstName:userName.firstName,
-  //   })
-  //   console.log("uid:", user.uid, "email:", user.email, "firstName:", userName.firstName, "lastName:", userName.lastName);
-  // };
-  
-  
+};
+
+// const splitHashtag = (postHashtags) => {
+
+//   let splitHashtagArray = postHashtags.split("#" || " ");
+//   let hashtagSearch = {
+//     firstHash: splitHashtagArray[0],
+//     secondhash: splitHashtagArray[1],
+//   };
+//   return hashtagSearch;
+// };
+// //vay guardar usuario en la base de datos despues de logarse
+// const saveUserToDatabaseAfterLogin = (user, userName) => {
+//   //Convertir las informaciones de google en um objecto
+//   db.collection("users").doc(user.uid).set({
+//     email:user.email,
+//     firstName:userName.firstName,
+//   })
+//   console.log("uid:", user.uid, "email:", user.email, "firstName:", userName.firstName, "lastName:", userName.lastName);
+// };
