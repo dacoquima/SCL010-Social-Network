5c4c1dedc68b13860ce1b43cfec54f7a2dfc6db5
Daniela Salermi <51088476+DaniSalermi@users.noreply.github.com>
2019-08-16 11:42:00 -0400

    Update template Profile

--- src/assets/views/templateProfile.js
+++ src/assets/views/templateProfile.js
@@ -1,7 +1,41 @@
 export const templateProfile = () => {
   const containerProfile = document.createElement("div");
   containerProfile.className = "containerProfile";
-  const contentProfile = `prueba`;
+  const contentProfile = `
+  <header class="secondHeader">
+    <img src="./assets/img/back.svg" class="iconInputBack" alt="boton para volver"/>
+    <img src="./assets/img/second-header.png" class="imgSecondHeader" alt="logo migra.me"/>
+  </header>
+  <main id = "templateProfile" clasS=mainLoginCreate">
+    <div class="profileTop" >
+      <img src="" class="profilePicture"/>
+      <h2 id="profileName"></h2>
+      <p id="profileProfession></p>
+      <p id="profileNacionality></p>
+      <p id="profileNacionality></p>
+      <p id="profileBio></p>
+    </div>
+    <div class="profileButtons">
+      <button id="editProfile" class="actionButtonRegular profileButton">Editar perfil</button>
+      <button id="logOut" class="actionButtonRegular profileButton">Cerrar sesión</button>
+    </div>
+    <div class="profileDetails">
+      <div class="profilePostDetails">
+        <p id="postCounter"></p>
+        <p>Publicaciones</p>
+      </div>
+      <div class="profileContactDetails">
+        <p id="contactCounter"></p>
+        <p>Contactos</p>
+      </div>
+    </div>
+    <hr/>
+
+    
+
+
+  </main>
+  `;
 
   containerProfile.innerHTML = contentProfile;
   return containerProfile;
