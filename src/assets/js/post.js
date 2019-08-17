a82ce4685836ef1bf7d1b3a6629fb888e2f59647
carolgmonteiro <carolgmonteiro@live.com>
2019-08-16 20:09:01 -0400

    like e informaciones del usuario
    Se puede dar like en los posts, aparece la informacion del usuario al crear un post

--- src/assets/js/post.js
+++ src/assets/js/post.js
@@ -1,4 +1,6 @@
-import { observer } from './auth.js'
+import {
+  observer
+} from './auth.js'
 
 
 const containerFeedPost = document.getElementById("root2");
@@ -6,6 +8,7 @@ const containerFeedPost = document.getElementById("root2");
 
 //guardamos firestore en variable
 let db = firebase.firestore();
+
 
 // funcion para crear posts
 export const createPost = () => {
@@ -20,83 +23,152 @@ export const createPost = () => {
   let postCategory = document.querySelector("select[name=slctCategory]").value;
   let postMesage = document.querySelector("textarea[name=postTxt]").value;
   console.log("category:", postCategory, "Mesage:", postMesage);
-//usamos esta funcion para obtener uid de usuario corriente
+  //usamos esta funcion para obtener uid de usuario corriente
   firebase.auth().onAuthStateChanged(user => {
     console.log(user);
     //obtenemos desde collecion users datos de usuario corriente con uid
     //db.collection('users').doc(user.uid).get().then(doc => {
-      //agrega un ID automatico
-      //console.log(doc._document.proto.fields);
+    //agrega un ID automatico
+    //console.log(doc._document.proto.fields);
     db.collection('posts').add({
-      //db.collection("users").doc(user.uid).set({
-      uid: user.uid,
-      //email: user.email,
-      //authorName: doc.data().user,
-      authorName: user.displayName,
-      photo: user.photoURL,
-      date: date,
-      category: postCategory,
-      message: postMesage
-      // like: 1,
-  
-    })
-    .then(function(doc) {
-      console.log("Document written with ID: ", doc.id);
-      window.location.hash='#/feed';
-      readPost();
-    })
-    .catch(function(error) {
-      console.error("Error adding document: ", error);
-    })
-   })
+        //db.collection("users").doc(user.uid).set({
+        uid: user.uid,
+        //email: user.email,
+        //authorName: doc.data().user,
+        authorName: user.displayName,
+        photo: user.photoURL,
+        date: date,
+        category: postCategory,
+        message: postMesage,
+        like: []
+
+      })
+      .then(function (doc) {
+        console.log("Document written with ID: ", doc.id);
+        window.location.hash = '#/feed';
+        readPost();
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      })
+  })
   //})
-  };
+};
 
 
 export const readPost = () => {
-    
+
   db.collection("posts").onSnapshot((querySnapshot) => {
-    containerFeedPost.innerHTML =  "";
+    containerFeedPost.innerHTML = "";
     querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data().message}`);
-        containerFeedPost.innerHTML += 
+      console.log(`${doc.id} => ${doc.data().message}`);
+      containerFeedPost.innerHTML +=
         `<main id = "templateWall" class="mainLoginCreate">
           <div class = "perfil">
             <img src=${doc.data().photo} class="imgAvatarPost" alt="avatar user"/>
             <h2>${doc.data().authorName}</h2>
           </div>
-          <p>${doc.id}</p>
-          <h2>${doc.data().category}</h2>
-          <textarea name="postTxtWallFinal" class="txtAreaStylePost" cols="40" rows="2">${doc.data().message}</textarea>
+          <h2 id= "categoryPost${doc.id}">${doc.data().category}</h2>
+          <p id = "messagePost${doc.id}" name="postTxtWallFinal" class="txtAreaStylePost">${doc.data().message}</p>
+          <textarea id = "editTextPost${doc.id}" name="postTxtWallFinal" class="txtAreaStylePost" cols="40" rows="2" style="display:none"></textarea>
           <button id='deletePost${doc.id}'>Borrar</button>
           <button id='editPost${doc.id}'>Editar</button>
+          <button id='savePost${doc.id}' style="display:none">Guardar</button>
+          <button id='likePost${doc.id}'>Like + </button>
+          <p>${doc.data().like.length} likes</p>
         </main>`
     });
     querySnapshot.forEach((doc) => {
       events(doc);
+      likeEvent(doc);
     })
-    });
-    return containerFeedPost;
-    };
-    
-//funcion para los eventos dentro de los posts   
-export const events = (doc) =>{
-  const btnDeletePost = document.getElementById('deletePost'+doc.id);
-  btnDeletePost.addEventListener("click", () => {
-    deletePost(doc.id);
-  })
+  });
+  return containerFeedPost;
+};
 
+//funcion para los eventos dentro de los posts   
+export const events = (doc) => {
+  if (firebase.auth().currentUser.uid === doc.data().uid) {
+    const btnDeletePost = document.getElementById('deletePost' + doc.id);
+    btnDeletePost.addEventListener("click", () => {
+      deletePost(doc.id);
+    })
+    const btnEditPost = document.getElementById('editPost' + doc.id);
+    btnEditPost.addEventListener("click", () => {
+      editPost(doc.id);
+    })
+  }
 }
+export const likeEvent = (doc) => {
+  const btnLikePost = document.getElementById('likePost' + doc.id);
+  btnLikePost.addEventListener("click", () => {
+    likePost(doc.id, doc.data().like);
+  })
+}
+
 
 // BORRAR POSTS
 export const deletePost = (id) => {
-  if(confirm("¿Seguro que quieres borrar tu publicación?")){
-db.collection("posts").doc(id).delete().then(function() {
-  containerFeedPost.innerHTML = "";
-  readPost();
-  console.log("Document successfully deleted!");
-}).catch(function(error) {
-  console.error("Error removing document: ", error);
-})
-}
+  if (confirm("¿Seguro que quieres borrar tu publicación?")) {
+    db.collection("posts").doc(id).delete().then(function () {
+      containerFeedPost.innerHTML = "";
+      readPost();
+      console.log("Document successfully deleted!");
+    }).catch(function (error) {
+      console.error("Error removing document: ", error);
+    })
+  }
 };
+
+// EDITAR POSTS
+export const editPost = (id) => {
+  db.collection("posts").doc(id).get().then(doc => {
+
+    //obtener el mensaje del post
+    document.getElementById(`editTextPost${doc.id}`).innerHTML = doc.data().message;
+    //aparece textArea para cambiar texto
+    document.getElementById(`editTextPost${doc.id}`).style.display = "flex";
+    //desaparece el P del mensaje
+    document.getElementById(`messagePost${doc.id}`).style.display = "none";
+    //escondemos el botón editar
+    document.getElementById(`editPost${doc.id}`).style.display = "none";
+    //escondemos boton de borrar
+    document.getElementById(`deletePost${doc.id}`).style.display = "none";
+    //mostrar botón guardar
+    document.getElementById(`savePost${doc.id}`).style.display = "flex";
+    //Evento que ocurre cuando se hace click en guardar
+    document.getElementById('savePost' + doc.id).addEventListener('click', () => {
+      let post = document.getElementById(`editTextPost${doc.id}`).value;
+      //Se hace update del post en la base de datos
+      let docRef = db.collection('posts').doc(id);
+      return docRef.update({
+          message: post
+        })
+        .then(() => {
+          document.getElementById(`messagePost${doc.id}`).style.display = "flex";
+          document.getElementById(`editTextPost${doc.id}`).style.display = "none";
+          document.getElementById(`deletePost${doc.id}`).style.display = "flex";
+          document.getElementById(`editPost${doc.id}`).style.display = "flex";
+          document.getElementById(`savePost${doc.id}`).style.display = "none";
+          console.log("Se actualizó el post")
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+    })
+  })
+};
+
+export const likePost = (id, like) => {
+  firebase.auth().onAuthStateChanged(user => {
+    console.log(user);
+    like.push(user.uid)
+    console.log(like)
+    let docRef = db.collection('posts').doc(id);
+    return docRef.update({
+      like: like
+    }).then((e) => {
+      console.log(e)
+    })
+  });
+}
--- src/assets/views/templatePost.js
+++ src/assets/views/templatePost.js
@@ -1,23 +1,30 @@
 //importar funcion que crea post
-import { createPost } from "../js/post.js";
-import { observer } from "./../js/auth.js";
+import {
+  createPost
+} from "../js/post.js";
+
 // import { saveUserToDatabaseAfterLogin } from "./../js/auth.js";
 const containerFeedPost = document.getElementById("root2");
 export const templatePost = () => {
-  containerFeedPost.innerHTML =  "";
-    console.log("Puede postar");
-    // let user = saveUserToDatabaseAfterLogin();
-    observer();
-        const containerPost = document.createElement("div");
-        containerPost.className = "containerPost";
-        const contentPost = `<header class="secondHeader">
-        <a id="feedIcon" class="iconInputBack" href="#/feed"><img src="./assets/img/back.svg"  alt="boton para volver"/></a>
-        <img src="./assets/img/second-header.png" class="imgSecondHeader" alt="logo migra.me"/>
-        </header>
-        <main id = "templatePost" class="mainLoginCreate">
-        <div><h1 class="secondarytextPost">Nueva publicación</h1></div>
-        <div class = "perfil">
-        <img src="./assets/img/user.svg" class="imgAvatar" alt="avatar user"/><h2>Ana María González</h2>
+  containerFeedPost.innerHTML = "";
+  let actualUser = firebase.auth().currentUser;
+  console.log(actualUser);
+
+  const containerPost = document.createElement("div");
+  containerPost.className = "containerPost";
+
+  const contentPost = `
+    <header class="secondHeader">
+      <a id="feedIcon" class="iconInputBack" href="#/feed"><img src="./assets/img/back.svg"  alt="boton para volver"/></a>
+      <img src="./assets/img/second-header.png" class="imgSecondHeader" alt="logo migra.me"/>
+    </header>
+    <main id = "templatePost" class="mainLoginCreate">
+      <div>
+        <h1 class="secondarytextPost">Nueva publicación</h1>
+      </div>
+      <div class = "perfil">
+        <img src="${actualUser.photoURL}" class="imgAvatar" alt="avatar user"/>
+        <h2>${actualUser.displayName}</h2>
         </div>
         <form class="formPost"> 
         <span class="iconInput1"> 
@@ -35,15 +42,13 @@ export const templatePost = () => {
           <textarea name="postTxt" class="txtAreaStyle" cols="40" rows="3" placeholder="¿Tienes algún dato?"></textarea>
           </form> 
             <button id='post' class="actionButtonRegular">Publicar</button>
-            </main>` 
-      
-            containerPost.innerHTML = contentPost;
-    
-        const btnPost = containerPost.querySelector("#post");
-        btnPost.addEventListener("click", () => { 
-          createPost();
-        });
-        return containerPost;
-      };
-    
-      
+            </main>`;
+  console.log("Llega aquí");
+  containerPost.innerHTML = contentPost;
+
+  const btnPost = containerPost.querySelector("#post");
+  btnPost.addEventListener("click", () => {
+    createPost();
+  });
+  return containerPost;
+};
